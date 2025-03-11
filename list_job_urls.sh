b0f388814b852196783e1761a75893444da87351
Perinban <p.perinban@gmail.com>
2025-03-11 18:41:33 +0100

    Updated Script

--- list_job_urls.sh
+++ list_job_urls.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # list_job_urls.sh
 
+cd /home/runner/work/WebScrapJobs/WebScrapJobs/
+
 # Check if the zip file exists in the job-urls directory
-if [ ! -f "./job-urls/job-urls.zip" ]; then
+if [ ! -f "/job-urls/job-urls.zip" ]; then
   echo "job-urls.zip not found in the job-urls directory!"
   exit 1
 fi
